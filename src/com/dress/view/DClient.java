f2bfb54028255c4e9aef5e1e357281d98f66ad73
Michael Anthony Cabrera <31862427+mcabrera9@users.noreply.github.com>
2020-04-25 13:57:46 -0500

    finalize visitor implementation in client

--- src/com/dress/view/DClient.java
+++ src/com/dress/view/DClient.java
@@ -13,42 +13,54 @@ public class DClient {
 		Dress casualDress = new CasualDress(new BasicDress());
 		//assemble the dress with a decorator
 		casualDress.assemble();
-		System.out.println();
-		
 		//calculate the price of the dress with a visitor
 		casualDress.setPrice(20);
 		casualDress.accept(visitor);
 		System.out.println("The price of this dress is $" + visitor.getFinalPrice());
+		System.out.println();
 		
 		Dress fancyDress = new FancyDress(new BasicDress());
 		//assemble the dress with a decorator
 		fancyDress.assemble();
-		System.out.println();
-		
-		//calculate the price of the dress with a visitor
 		fancyDress.setPrice(600);
 		fancyDress.accept(visitor);
+		//calculate the price of the dress with a visitor
 		System.out.println("The price of this dress is $" + visitor.getFinalPrice());
+		System.out.println();
 		
 		Dress sportyDress = new SportyDress(new BasicDress());
+		//assemble the dress with a decorator
 		sportyDress.assemble();
+		sportyDress.setPrice(80);
+		sportyDress.accept(visitor);
+		//calculate the price of the dress with a visitor
+		System.out.println("The price of this dress is $" + visitor.getFinalPrice());
 		System.out.println();
 		
 		Dress sportyFancyDress = new SportyDress(new FancyDress(new BasicDress()));
+		//assemble the dress with a decorator
 		sportyFancyDress.assemble();
+		sportyFancyDress.setPrice(200);
+		sportyFancyDress.accept(visitor);
+		//calculate the price of the dress with a visitor
+		System.out.println("The price of this dress is $" + visitor.getFinalPrice());
 		System.out.println();
 		
 		Dress casualFancyDress = new CasualDress(new FancyDress(new BasicDress()));
+		//assemble the dress with a decorator
 		casualFancyDress.assemble();
+		casualFancyDress.setPrice(150);
+		casualFancyDress.accept(visitor);
+		//calculate the price of the dress with a visitor
+		System.out.println("The price of this dress is $" + visitor.getFinalPrice());
 		System.out.println();
 		
 		Dress casualSportyDress = new CasualDress(new SportyDress(new BasicDress()));
-		//assemble the dress
+		//assemble the dress with a decorator
 		casualSportyDress.assemble();
-		System.out.println();
-		//calculate the price of the dress
 		casualSportyDress.setPrice(55);
 		casualSportyDress.accept(visitor);
+		//calculate the price of the dress with a visitor
 		System.out.println("The price of this dress is $" + visitor.getFinalPrice());
 		
 		
