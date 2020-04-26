4c6bb4c059ee47fef81e33f3847853457cd28f8b
Michael Anthony Cabrera <31862427+mcabrera9@users.noreply.github.com>
2020-04-26 15:44:27 -0500

    add visitor tests

--- src/com/dress/tests/VisitorTest.java
+++ src/com/dress/tests/VisitorTest.java
@@ -1,5 +1,48 @@
 package com.dress.tests;
 
-public class VisitorTest {
+import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.dress.model.basicdress.BasicDress;
+import com.dress.model.basicdress.Dress;
+import com.dress.model.dressdecorator.CasualDress;
+import com.dress.model.dressdecorator.FancyDress;
+import com.dress.model.visitor.ShoppingVisitorImpl;
+
+public class VisitorTest {
+	ShoppingVisitorImpl visitor = new ShoppingVisitorImpl();
+	Dress casualDress = new CasualDress(new BasicDress());
+	Dress fancyDress = new FancyDress(new BasicDress());
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		casualDress.assemble();
+		fancyDress.assemble();
+		
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testNoDiscount() {
+	//test to visitor without a discount
+	casualDress.setPrice(20);
+	casualDress.accept(visitor);
+	assertEquals(20, visitor.getFinalPrice());
+		
+	}
+	@Test
+	void testDiscount() {
+	//test to visitor calculating a discount
+	fancyDress.setPrice(55);
+	fancyDress.accept(visitor);
+	assertEquals(50, visitor.getFinalPrice());
+		
+	}
+	
 }
